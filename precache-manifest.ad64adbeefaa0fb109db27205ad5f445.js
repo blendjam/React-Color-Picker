be02d1947049094ac9b199b43ccbecd9abc63562
llsanketll <sanket.lamsal@gmail.com>
2020-06-26 14:38:58 +0545

    Updates

---
+++ precache-manifest.ad64adbeefaa0fb109db27205ad5f445.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a20054fd1abdcec61a2939f51823c89",
+    "url": "/React-Color-Picker/index.html"
+  },
+  {
+    "revision": "b4fc238a139312710517",
+    "url": "/React-Color-Picker/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "3ac5dad20442b078fc5f",
+    "url": "/React-Color-Picker/static/css/main.3364c02c.chunk.css"
+  },
+  {
+    "revision": "b4fc238a139312710517",
+    "url": "/React-Color-Picker/static/js/2.021b597d.chunk.js"
+  },
+  {
+    "revision": "5dbf9415e481a23d65662fd7dec5f7a6",
+    "url": "/React-Color-Picker/static/js/2.021b597d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac5dad20442b078fc5f",
+    "url": "/React-Color-Picker/static/js/main.60d2ace1.chunk.js"
+  },
+  {
+    "revision": "44cd8fd15447067493a8",
+    "url": "/React-Color-Picker/static/js/runtime-main.276e87fa.js"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/React-Color-Picker/static/media/X.b0200bf7.svg"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/React-Color-Picker/static/media/arrow.c66b213d.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/React-Color-Picker/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "d4547011f69a9a5700f9641ae79007e8",
+    "url": "/React-Color-Picker/static/media/plus-circle.d4547011.svg"
+  },
+  {
+    "revision": "e688762277044cb8758d1ee0a5909169",
+    "url": "/React-Color-Picker/static/media/trash.e6887622.svg"
+  }
+]);
